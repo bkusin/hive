563e769f0d737d30a1530e1fd5ce6383cf8a44b1
Benjamin Kusin <benjaminkusin@gmail.com>
2026-08-26 21:02:39 -0400

    Reimplement as vector of pointers to blocks. Initially very simple.

--- src/hive.rs
+++ src/hive.rs
@@ -1,113 +1,102 @@
-use std::{mem::MaybeUninit, ops::{Index, IndexMut}};
+use std::{
+    mem::{MaybeUninit, uninitialized}, ops::{Index, IndexMut},
+};
 
 const DEFAULT_CAP: usize = 16;
 
-struct Vacancy {
+struct Handle {
     block_idx: usize,
     block_offset: usize,
+    // TODO: Generations
+}
+
+struct Block<T> {
+    data: [MaybeUninit<T>; DEFAULT_CAP],
+    len: usize,
+    liveness: [bool; DEFAULT_CAP],
+    free_list: Vec<usize>,         // stack of deleted slots
+}
+
+impl<T> Block<T> {
+    fn new() -> Self {
+        Self {
+            data: [ const { MaybeUninit::uninit() }; DEFAULT_CAP],
+            len: 0,
+            liveness: [false; DEFAULT_CAP],
+            free_list: Vec::new(),
+        }
+    }
+
+    fn insert(&mut self, value: T) -> usize {
+        // reuse an empty slot first
+        let mut index = 
+        if !self.free_list.is_empty() {
+            self.free_list.pop().unwrap()
+        }
+        else {
+            // either the block is completely empty or completely full
+            // the caller already checks the latter case
+            0
+        };
+            
+        self.data[index].write(value);
+        self.len += 1;
+        index
+    }
 }
 
 struct Hive<T> {
-    block_size: usize,
-    data: Vec<Box<[MaybeUninit<T>; DEFAULT_CAP]>>,
-    liveness: Vec<bool>,
-    vacancies: Vec<Vacancy>,
-    len: usize,
-    capacity: usize,
+  //  block_size: usize,
+    data: Vec<Box<Block<T>>>,
+    blocks_with_vacancies: usize,
 }
 
 impl<T> Hive<T> {
-    
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
-            block_size: DEFAULT_CAP,
-            data: vec![Box::<[MaybeUninit<T>; DEFAULT_CAP]>::new( [const { MaybeUninit::uninit() }; DEFAULT_CAP] )],
-            liveness: vec![true; DEFAULT_CAP],
-            vacancies: Vec::<Vacancy>::with_capacity(DEFAULT_CAP),
-            len: 0,
-            capacity: DEFAULT_CAP,
+           data: Vec::new(),
+           blocks_with_vacancies: 0,
         }
     }
 
-    fn push(&mut self, value: T) {
-        let mut block_idx = self.len / self.block_size;
-        let mut block_offset = self.len % self.block_size;
-
-        // append, fill vacancy, or expand
-        if self.len == self.capacity {
-            if !self.vacancies.is_empty() {
-                let vacancy = self.vacancies.last().unwrap();
-                block_idx = vacancy.block_idx;
-                block_offset = vacancy.block_offset;
-            }
-            else {
-                // expand
-                self.data.push(Box::<[MaybeUninit<T>; DEFAULT_CAP]>::new([const { MaybeUninit::uninit() }; DEFAULT_CAP]));
-                self.capacity += DEFAULT_CAP;
-                self.liveness.extend_from_slice(&[false; DEFAULT_CAP]);
-                block_idx += 1;
-                block_offset = 0;
+    pub fn insert(&mut self, value: T) -> Handle {
+        // if there are no blocks with vacancies, create a new block
+        if self.blocks_with_vacancies == 0 {
+            self.data.push(Box::new(Block::<T>::new()));
+            self.blocks_with_vacancies = 1;
+        }
+    
+        // look for a block to insert into
+        for block in self.data.iter_mut().enumerate() {
+            if block.1.len < DEFAULT_CAP {
+                let idx = block.1.insert(value);
+                return Handle{ block_idx: block.0, block_offset: idx };
             }
         }
 
-        (self.data[block_idx])[block_offset].write(value);
-        self.liveness[self.len] = true;
-        self.len += 1;
+        unreachable!("Hive should always have a block with space");
     }
 
-    fn delete(&self) {
-        todo!();
+    pub fn get(&self, handle: Handle) -> &T {
+        // TODO deal with invalid handle, generation
+        unsafe {
+            self.data[handle.block_idx].data[handle.block_offset].assume_init_ref()
+        }
     }
 
-    fn len(&self) -> usize {
-        self.len
+    pub fn len(&self) -> usize {
+        let mut len = 0;
+        for block in &self.data {
+            len += block.len;
+        }
+        len
     }
 
-    fn capacity(&self) -> usize {
-        self.capacity
+    pub fn capacity(&self) -> usize {
+        self.data.capacity() * DEFAULT_CAP
     }
+
 }
-
-impl<T> Index<usize> for Hive<T> {
-    type Output = T;
-
-    fn index(&self, index: usize) -> &T {
-        if index >= self.capacity { panic!(); }
-
-        let block_idx = index / self.block_size;
-        let idx = index - (block_idx * self.block_size);
-
-        unsafe { (self.data[block_idx])[idx].assume_init_ref() }
-    }
-}
-
-impl<T> IndexMut<usize> for Hive<T> {
-   
-    fn index_mut(&mut self, index: usize) -> &mut T {
-        if index >= self.capacity { panic!(); }
-
-        let block_idx = index / self.block_size;
-        let idx = index - (block_idx * self.block_size);
-
-        unsafe { (self.data[block_idx])[idx].assume_init_mut() }
-    }
-}
-
-// TODO:
-/*
-indexing math needs to "skip over" soft-deleted elements
-similarly, since we don't actually delete anything, we need to handle dangling references because Rust's lifetime and borrowing rules
-won't help with soft-deletes.
-
-This means we need a "virtual length" as if the structure is compacted to close "deletes"
-How does C++26 std::hive do it?
-The existence of holes at arbitrary locations means the only way to get the correct live index is to scan linearly.
-So std::hive does not have indexing. But iterators are valid until the item is deleted or the container is sorted/cleared/resized.
-
-implement Iterator
-
-Benchmark latency stability
-*/
 
 
 mod test {
@@ -119,30 +108,39 @@ mod test {
     }
 
     #[test]
-    fn test_push() {
+    fn test_insert() {
         let mut hive: Hive<i32> = Hive::new();
-        hive.push(42);
+        let handle = hive.insert(42);
 
-        assert_eq!(hive[0], 42);
+        assert_eq!(*hive.get(handle), 42);
+    }
+
+    #[test]
+    fn test_delete() {
+        let mut hive: Hive<i32> = Hive::new();
+        let handle = hive.insert(42);
+        assert_eq!(hive.len(), 1);
+
+    //    hive.delete(handle);
+    //    assert_eq!(hive.len(), 0);
     }
 
     #[test]
     fn test_expand() {
         let mut hive: Hive<i32> = Hive::new();
         for i in 0..18 {
-            hive.push(i as i32);
+            hive.insert(i as i32);
         }
 
         // should access second block
         assert_eq!(hive.capacity(), 32);
         assert_eq!(hive.len(), 18);
-        assert_eq!(hive[17], 17);
-        assert_eq!(&hive.liveness[0..18], &[true; 18]);
+     //   assert_eq!(hive[17], 17);
+     //   assert_eq!(&hive.liveness[0..18], &[true; 18]);
     }
 
     #[test]
     fn fill_vacancy() {
         todo!()
     }
-
 }
